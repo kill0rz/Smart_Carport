1016dc7f271afa0c0f0cc029837fc82c96bda649
kill0rz <kill0rz@users.noreply.github.com>
2017-01-10 19:17:17 +0100

    removed title

--- Install/Files_to_upload/var/www/html/index.php
+++ Install/Files_to_upload/var/www/html/index.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel=stylesheet href="./bootstrap.min.css">
-	<title>Smart CarPort 84</title>
+	<title>Smart CarPort</title>
 </head>
 <body>
 	<div class="container">
