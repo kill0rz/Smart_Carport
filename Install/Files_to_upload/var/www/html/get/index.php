c1e8a7f7eb7066dddf8881953d883dd14cf3382e
kill0rz <kill0rz@users.noreply.github.com>
2017-08-21 17:02:41 +0200

    added feucht and temp to get method

--- Install/Files_to_upload/var/www/html/get/index.php
+++ Install/Files_to_upload/var/www/html/get/index.php
@@ -12,6 +12,9 @@ if ($debug) {
 while ($row = mysqli_fetch_object($result)) {
 	$new_row = $row;
 	$new_row->is_running = trim(shell_exec("gpio -g read 23"));
+	preg_match_all("/Temp=(.*) Humidity=(.*)/", trim(shell_exec("sudo /root/sh/sensor.sh")), $matches);
+	$new_row->temp = (isset($matches[1][0])) ? trim(str_replace("*", "&deg;C", $matches[1][0])) : "nAn";
+	$new_row->feucht = (isset($matches[2][0])) ? trim($matches[2][0]) : "nAn";
 	echo json_encode($new_row);
 	exit();
 }
